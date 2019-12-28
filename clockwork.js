9164cb0370b16a2df0a4f27eac9a8ffa579fee9e
Paul Roub <paul@roub.net>
2019-12-28 18:12:30 -0500

    Let's just test time-to-words now, not the display

--- clockwork.js
+++ clockwork.js
@@ -1,30 +1,7 @@
 module.exports = {
-    highlightTime() {
+    timeWords() {
         return [
-            [
-                "*it*", "*is*", "half", "ten"
-            ],
-            [
-                "quarter", "twenty"
-            ],
-            [
-                "five", "minutes", "to"
-            ],
-            [
-                "past", "one", "three"
-            ],
-            [
-                "two", "four", "five"
-            ],
-            [
-                "six", "seven", "eight"
-            ],
-            [
-                "nine", "ten", "eleven"
-            ],
-            [
-                "*twelve*", "*o'clock*"
-            ]
+            "it", "is", "tweleve", "o'clock"
         ];
     }
 }
--- trivial.test.js
+++ trivial.test.js
@@ -3,31 +3,8 @@ const clockwork = require('./clockwork');
 test('high noon', () => {
     const time = '12:00:00';
     const expected = [
-        [
-            "*it*", "*is*", "half", "ten"
-        ],
-        [
-            "quarter", "twenty"
-        ],
-        [
-            "five", "minutes", "to"
-        ],
-        [
-            "past", "one", "three"
-        ],
-        [
-            "two", "four", "five"
-        ],
-        [
-            "six", "seven", "eight"
-        ],
-        [
-            "nine", "ten", "eleven"
-        ],
-        [
-            "*twelve*", "*o'clock*"
-        ]
+        "it", "is", "tweleve", "o'clock"
     ];
 
-    expect(clockwork.highlightTime(time)).toStrictEqual(expected);
+    expect(clockwork.timeWords(time)).toStrictEqual(expected);
 });
