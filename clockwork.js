02a85d28c295bec5edf71931cc857c2928ebf694
Paul Roub <paul@roub.net>
2019-12-28 18:10:45 -0500

    Simplest, dumbest passage of our noon test

--- clockwork.js
+++ clockwork.js
@@ -1,5 +1,30 @@
 module.exports = {
     highlightTime() {
-        return null;
+        return [
+            [
+                "*it*", "*is*", "half", "ten"
+            ],
+            [
+                "quarter", "twenty"
+            ],
+            [
+                "five", "minutes", "to"
+            ],
+            [
+                "past", "one", "three"
+            ],
+            [
+                "two", "four", "five"
+            ],
+            [
+                "six", "seven", "eight"
+            ],
+            [
+                "nine", "ten", "eleven"
+            ],
+            [
+                "*twelve*", "*o'clock*"
+            ]
+        ];
     }
 }
