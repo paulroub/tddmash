86880d3d31ce2ba2a928736b1b341f91183b9cd3
Paul Roub <paul@roub.net>
2019-12-29 19:04:43 -0500

    Add coverage test for a potential oddball case

--- highlights.test.js
+++ highlights.test.js
@@ -2,7 +2,7 @@ const {
     highlights
 } = require('./clockwork');
 
-test('highlight in order', () => {
+test('highlight noon', () => {
     const timewords = ["it", "is", "twelve", "o'clock"];
     const expected = [
         ["*it", "*is", "half", "ten"],
@@ -13,6 +13,22 @@ test('highlight in order', () => {
         ["six", "seven", "eight"],
         ["nine", "ten", "eleven"],
         ["*twelve", "*o'clock"]
+    ];
+
+    expect(highlights(timewords)).toStrictEqual(expected);
+});
+
+test('highlight twenty-five after five', () => {
+    const timewords = ["it", "is", "twenty", "five", "minutes", "past", "five"];
+    const expected = [
+        ["*it", "*is", "half", "ten"],
+        ["quarter", "*twenty"],
+        ["*five", "*minutes", "to"],
+        ["*past", "one", "three"],
+        ["two", "four", "*five"],
+        ["six", "seven", "eight"],
+        ["nine", "ten", "eleven"],
+        ["twelve", "o'clock"]
     ];
 
     expect(highlights(timewords)).toStrictEqual(expected);
